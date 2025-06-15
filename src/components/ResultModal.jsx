7dd2144cbc427caa0c7f2bdc36a431b8c97d7e0d
Yashasvi Srivastava <yksrivastava944@gmail.com>
2025-06-15 12:36:56 +0530

    refactor the code to view score and also see the use of createPortal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,16 @@
 import { useImperativeHandle, useRef } from "react";
-
-export default function ResultModal({ ref, result, targetTime }) {
+import { createPortal } from "react-dom";
+export default function ResultModal({
+  ref,
+  targetTime,
+  remainingTime,
+  onReset,
+}) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainignTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round(1 - remainingTime / (targetTime * 1000)) * 100;
   useImperativeHandle(ref, () => {
     return {
       open() {
@@ -9,19 +18,23 @@ export default function ResultModal({ ref, result, targetTime }) {
       },
     };
   });
-  return (
+  // createPOrtal is used to render the component to the choice of dom, below we have attached to the div having id as modal containing in index.html
+  return createPortal(
     <dialog className="result-modal" ref={dialog}>
-      <h2>You {result}</h2>
+      {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainignTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 }
 
